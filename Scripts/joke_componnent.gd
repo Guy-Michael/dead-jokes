9d5479714f74455274538621fdbd5d76152f410c
itaisinger <itai5000si2@gmail.com>
2024-01-26 22:25:30 +0200

    dad

--- Scripts/joke_componnent.gd
+++ Scripts/joke_componnent.gd
@@ -5,19 +5,19 @@ var is_inside_dropable = false	#Is the object in a dropabale area
 var body_ref	#Refrence to the dropable area when the object enters it
 var offset: Vector2		#For maintaing the position of the object relative to the mouse pos
 var inital_pos: Vector2		#Keeps the inital position of the object
-@export var component_type: gloabls.Joke_Component 
+@export var component_type: globals.Joke_Component 
 
 func _process(_delta):
 	if draggable:
 		if Input.is_action_just_pressed("click"):
 			inital_pos = global_position
 			offset = get_global_mouse_position() - global_position
-			gloabls.is_dragging = true
+			globals.is_dragging = true
 		
 		if Input.is_action_pressed("click"):
 			global_position = get_global_mouse_position() - offset
 		elif Input.is_action_just_released("click"):
-			gloabls.is_dragging = false
+			globals.is_dragging = false
 			var tween = get_tree().create_tween()
 			if is_inside_dropable:
 				tween.tween_property(self, "position", body_ref.position, 0.2).set_ease(Tween.EASE_OUT)		
@@ -26,12 +26,12 @@ func _process(_delta):
 func _on_area_2d_mouse_entered():
 	#Making sure we are dragging only one object
 	print_debug("mouse entered")
-	if not gloabls.is_dragging:
+	if not globals.is_dragging:
 		draggable = true
 		scale = Vector2(1.05, 1.05)		#Scaling the sprite (more for the visual effect)
 		
 func _on_area_2d_mouse_exited():
-	if not gloabls.is_dragging:
+	if not globals.is_dragging:
 		draggable = false
 		scale = Vector2(1, 1)
 		
